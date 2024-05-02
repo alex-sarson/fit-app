8d1f6820a6d9681b0aac012dc0c46a529e49c40d
alex-sarson <alex.luke.sarson@gmail.com>
2024-05-02 21:57:17 +0100

    implemented password hashing in register handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,17 @@
 import Role from '../models/Role.js';
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res, next) => {
   const role = await Role.find({ role: 'User' });
+  const salt = await bcrypt.genSalt(10);
+  const hashPass = await bcrypt.hash(req.body.password, salt);
   const newUser = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: hashPass,
     roles: role,
   });
   await newUser.save();
